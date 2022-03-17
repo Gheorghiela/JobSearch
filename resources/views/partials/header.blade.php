fc3fc66c3ba1298be91e3b2f21cb8156c3eef30d
Georgeta <ciorbageorgeta@gmail.com>
2022-03-17 22:18:09 +0200

    Add views and styles

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header id="header" id="home">
+    <div class="container">
+        <div class="row align-items-center justify-content-between d-flex">
+          <div id="logo">
+{{--            <a href="{{ route('home') }}"><img src="{{ asset('img/logo.png') }}" alt="" title="" /></a>--}}
+             <a href="{{ route('home') }}"><span style="font-size: 24px; font-weight: bold; color: white;">Job Search</span></a>
+          </div>
+        </div>
+    </div>
+</header>
